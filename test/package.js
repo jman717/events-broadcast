fb1554434a8d0801593d0d6a341d32a096e19a6a
jman717 <jim.manton@skybeam.com>
2025-10-01 23:48:13 -0600

    YML support for various ubuntu and windows versions, force publish new version

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "jim manton"
   },
-  "version": "2.1.3",
+  "version": "2.1.4",
   "bundleDependencies": true,
   "dependencies": {
     "chai": "^5.0.0",
