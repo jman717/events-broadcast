2975aebfd4d37a36cad0a2374745f936506c5faa
jman717 <jim.manton@skybeam.com>
2025-10-01 22:37:49 -0600

    Update yml 22

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "jim manton"
   },
-  "version": "2.0.20",
+  "version": "2.0.21",
   "bundleDependencies": true,
   "dependencies": {
     "chai": "^5.0.0",
