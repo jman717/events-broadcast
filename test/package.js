0b31b801ba059df9f102e5ec68123e73df2ed95a
jman717 <jim.manton@skybeam.com>
2025-10-03 12:35:18 -0600

    Package-lock fix added.

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "jim manton"
   },
-  "version": "3.0.6",
+  "version": "3.0.7",
   "bundleDependencies": false,
   "dependencies": {
     "chai": "^5.0.0",
