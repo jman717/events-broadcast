2885f68d050f4b108b46094a1c8c7f63e4875443
jman717 <jim.manton@skybeam.com>
2025-10-04 16:28:37 -0600

    Test yml npm-publish reset 1.

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "jim manton"
   },
-  "version": "3.0.32",
+  "version": "3.0.33",
   "bundleDependencies": false,
   "dependencies": {
     "chai": "^5.0.0",
