b69532f32eb21a482acd346f39807dbb14929eab
jman717 <jim.manton@skybeam.com>
2025-10-04 16:48:10 -0600

    Test yml npm-publish GITHUB_TOKEN 1.

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "jim manton"
   },
-  "version": "3.0.33",
+  "version": "3.0.34",
   "bundleDependencies": false,
   "dependencies": {
     "chai": "^5.0.0",
