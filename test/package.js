47f3eb71c60d5b6fa9593f99c00fa8fb0e439433
jman717 <jim.manton@skybeam.com>
2025-10-03 22:45:35 -0600

    Test yml npm-publish 26.

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "jim manton"
   },
-  "version": "3.0.29",
+  "version": "3.0.30",
   "bundleDependencies": false,
   "dependencies": {
     "chai": "^5.0.0",
