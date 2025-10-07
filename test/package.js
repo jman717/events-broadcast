238397ae1e9383fd915f0b3a7eeb9a373bc3a364
jman717 <jim.manton@skybeam.com>
2025-10-06 20:14:11 -0600

    Test yml Publish To NPM CI_COMMIT_REF_NAME 13.00

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "jim manton"
   },
-  "version": "3.0.87",
+  "version": "3.0.88",
   "bundleDependencies": false,
   "dependencies": {
     "chai": "^5.0.0",
