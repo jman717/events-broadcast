8d373963e39f33d2dab1e2bc9c9b563415eee046
jman717 <jim.manton@skybeam.com>
2025-10-09 20:42:34 -0600

    -v 3.0.147

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "jim manton"
   },
-  "version": "3.0.146",
+  "version": "3.0.147",
   "bundleDependenies": [],
   "dependencies": {
     "chai": "^5.0.0",
